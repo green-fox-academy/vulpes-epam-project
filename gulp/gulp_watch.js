85988cd8f95b86ca030b25cabbbf4996e8731f39
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-29 15:16:39 +0100

    add 'jshint' gulp task to gulp 'watch'

--- gulp/gulp_watch.js
+++ gulp/gulp_watch.js
@@ -3,11 +3,11 @@
 var batch = require('gulp-batch');
 var gulp = require('gulp');
 var watch = require('gulp-watch');
-var sass = require('gulp-sass');
 
 gulp.task('watch', function () {
     watch('**/*.js', batch(function (events, done) {
         gulp.start('jasmine', done);
+        gulp.start('jshint', done);
     }));
     watch('**/*.scss', batch(function (events, done) {
         gulp.start('sass', done);
