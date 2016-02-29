f3e51c62dca0eaf3ba502ddc0eca447c8fd6d99c
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-02-29 14:39:38 +0100

    change postgres database name, username and password in config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   DEFAULT_PORT: process.env.PORT || 3000,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:epam1@localhost/postgres',
+  DATABASE_URL: process.env.DATABASE_URL ||
+    'postgres://postgres:postgres@localhost/epam_interviewer',
   DEFAULT_LOGGING_LEVEL: process.env.DEFAULT_LOGGING_LEVEL || 'debug',
   EXPRESS_SESSION_CONFIG: {
     secret: 'keyboard cat',
