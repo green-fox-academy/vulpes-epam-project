fca7e8b34b4f521d69fb953a0752f9d92dd7ab3e
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-03-01 17:11:37 +0100

    delete supertest on registration endpoint

--- spec/server/userControllerSpec.js
+++ spec/server/userControllerSpec.js
@@ -14,25 +14,6 @@ describe('Controller', function () {
     app = createServer(connection);
   });
 
-  describe('POST api/register', function () {
-    it('should respond with json', function (done) {
-      var user = { email: 'smth@fox.com', password: '1234' };
-      request(app)
-        .post('/api/register')
-        .send(user)
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(function (err, res) {
-          if (err) {
-            done.fail(err);
-          } else {
-            expect(res.body).toEqual([{}]);
-            done();
-          }
-        });
-    });
-  });
-
   describe('GET /api/users', function () {
     it('should respond with json', function (done) {
       request(app)
