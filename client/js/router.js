bb06b3d01b9eaf04a3813ba56a354bfddd8b95c1
Vrandi <vrabely.andrea@gmail.com>
2016-03-02 17:41:21 +0100

    delete log function, add new script to html

--- client/js/router.js
+++ client/js/router.js
@@ -42,12 +42,4 @@ angular.module('myapp')
           pageTitle: 'Users',
         },
       });
-  })
-  .run(function ($rootScope, $http) {
-    $rootScope.$on('$stateChangeStart',
-      function (event, toState) {
-        window.document.title = toState.data.pageTitle;
-        var logMessage = { level: 'info', toState: toState.url };
-        $http.post('/api/log', logMessage);
-      });
   });
