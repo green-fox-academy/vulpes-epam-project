1c6164711489364fb9915f88ca829ee7170f5b45
György Csadó <gyorgy.csado@gmail.com>
2016-03-02 09:33:08 +0100

    add question data table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE DATABASE epam_interviewer;
 \connect epam_interviewer;
 CREATE TABLE heartbeat (ok BOOLEAN);
 CREATE TABLE USERS (user_id SERIAL, email TEXT NOT NULL, password TEXT NOT NULL, isAdmin BOOLEAN DEFAULT false, UNIQUE (email), PRIMARY KEY (user_id));
+CREATE TABLE QUESTIONS (question_id SERIAL, type TEXT NOT NULL, content TEXT NOT NULL, UNIQUE (type, content), PRIMARY KEY (question_id));
