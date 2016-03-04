fc3308b93b456e5be00af2431f943b2d8e68e504
Vrandi <vrabely.andrea@gmail.com>
2016-03-04 13:56:13 +0100

    Configure frontend logging

--- server/log_controller.js
+++ server/log_controller.js
@@ -3,6 +3,8 @@
 var logger = require('./log.js')();
 
 function createLogController() {
+  var frontendLogLevel = 'info';
+  var levels = ['debug', 'info', 'warn', 'error'];
 
   function logRequest(req, res, next) {
     var logLevel = 'info';
@@ -15,7 +17,19 @@ function createLogController() {
     var logLevel = req.body.level;
     var logMessage = `NEW FRONTEND EVENT, ${req.body.message}`;
     logger.message(logLevel, logMessage);
-    res.status(200).json({ 'Logging:': 'Success' });
+    if (isValidLevel(logLevel)) {
+      return res.status(200).json({
+        level: logLevel,
+        date: new Date().toISOString(),
+        message: logMessage,
+      });
+    }
+
+    res.status(200).json({ 'Logging:': 'Faild' });
+  }
+
+  function isValidLevel(level) {
+    return levels.indexOf(level) >= levels.indexOf(frontendLogLevel);
   }
 
   return {
