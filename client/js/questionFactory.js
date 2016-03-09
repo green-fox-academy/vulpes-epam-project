11ca6749b0da034d818d51065ef2114087e5433f
Edina Frank <frankedus@gmail.com>
2016-03-09 15:09:16 +0100

    same font-family in textarea and input

--- client/js/questionFactory.js
+++ client/js/questionFactory.js
@@ -3,11 +3,6 @@
 var EPAM = require('./main');
 
 EPAM.factory('question', function ($http) {
-    var question = {
-      content: '',
-      type: '',
-    };
-
     function addNewQuestion(newQuestion) {
       return $http.post('/api/questions', newQuestion);
     }
