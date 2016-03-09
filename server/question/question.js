99489c6aa67c5ea63e14d1cfebd06f0fc3dd82ba
György Csadó <gyorgy.csado@gmail.com>
2016-03-09 23:20:59 +0100

    fix(question) GET response format

--- server/question/question.js
+++ server/question/question.js
@@ -6,32 +6,38 @@ function createQuestion(query) {
 
   function getAllQuestion(request, response) {
     query.getAllQuestion(function (err, result) {
-      handleResponse(err, result, response);
+      handleResponse(err, result, response, 'get');
     });
   }
 
   function postQuestion(request, response) {
     query.postQuestion(request.body, function (err, result) {
-      handleResponse(err, result, response);
+      handleResponse(err, result, response, null);
     });
   }
 
   function putQuestion(request, response) {
     query.putQuestion(request.body, request.params.id, function (err, result) {
-      handleResponse(err, result, response);
+      handleResponse(err, result, response, null);
     });
   }
 
   function deleteQuestion(request, response) {
     query.deleteQuestion(request.params.id, function (err, result) {
-      handleResponse(err, result, response);
+      handleResponse(err, result, response, null);
     });
   }
 
-  function handleResponse(err, result, response) {
+  function handleResponse(err, result, response, method) {
     if (err) {
       logger.message('error', 'DATABASE CONNECTION ERROR');
       response.status(503).json({ 'Connection Error:': err });
+    } else if (method === 'get') {
+      logger.message('info', 'SUCCESSFUL DATABASE QUERY');
+      response.status(200).json({
+        questions: result.rows,
+        status: 'ok',
+      });
     } else {
       logger.message('info', 'SUCCESSFUL DATABASE QUERY');
       response.status(200).json(result.rows);
