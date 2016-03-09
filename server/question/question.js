2fe34e310d5c3aecdb90ead6ecca7cb539290fb2
Edina Frank <frankedus@gmail.com>
2016-03-09 13:56:22 +0100

    delete questions from question list

--- server/question/question.js
+++ server/question/question.js
@@ -33,13 +33,8 @@ function createQuestion(query) {
       logger.message('error', 'DATABASE CONNECTION ERROR');
       response.status(503).json({ 'Connection Error:': err });
     } else {
-      if (result.rows.length === 0) {
-        logger.message('warn', 'ITEM NOT FOUND IN DATABASE');
-        response.status(404).json(result.rows);
-      } else {
-        logger.message('info', 'SUCCESSFUL DATABASE QUERY');
-        response.status(200).json(result.rows);
-      }
+      logger.message('info', 'SUCCESSFUL DATABASE QUERY');
+      response.status(200).json(result.rows);
     }
   }
 
