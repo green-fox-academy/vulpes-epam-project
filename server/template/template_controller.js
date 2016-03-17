157ed42f3b22ae37ee5d1fc894db2ba1ab6a9033
Vrandi <vrabely.andrea@gmail.com>
2016-03-17 16:53:48 +0100

    Refactor controller

--- server/template/template_controller.js
+++ server/template/template_controller.js
@@ -5,7 +5,7 @@ function createTemplateController(queries) {
 
   function getAllTemplates(req, res) {
     queries.getTemplates(function (err, result) {
-      handleResponse(err, result, res);
+      handleResponse(err, schema.allTemplatesSchema(result), res);
     });
   }
 
@@ -13,9 +13,7 @@ function createTemplateController(queries) {
     var errorMessage = false;
     queries.postTemplate(req.body, function (err, result, response) {
       if (err) {
-        response.status(503).json({
-          errorMessage: 'Database error. Please try again later.',
-        });
+        dbErrorResponse(response);
       } else {
         req.body.schema.forEach(function (elem) {
           queries.postTemplateSetup(elem, req.body.title, function (err) {
@@ -24,7 +22,7 @@ function createTemplateController(queries) {
           });
         });
 
-        postHandleResponse(errorMessage, 'Post ok', res);
+        handleResponse(errorMessage, 'Post ok', res);
       }
     });
   }
@@ -32,12 +30,10 @@ function createTemplateController(queries) {
   function deleteTemplate(req, res) {
     queries.deleteTemplate(req.params.id, function (err, result, response) {
       if (err) {
-        response.status(503).json({
-          errorMessage: 'Database error. Please try again later.',
-        });
+        dbErrorResponse(response);
       } else {
-        queries.deleteTemplateSetup(req.params.id, function (err, result) {
-          handleResponse(err, result, res);
+        queries.deleteTemplateSetup(req.params.id, function (err) {
+          handleResponse(err, 'Delete ok', res);
         });
       }
     });
@@ -46,21 +42,9 @@ function createTemplateController(queries) {
   function getTemplateQuestions(req, res) {
     queries.getTemplateSetup(req.params.id, function (err, result) {
       if (err) {
-        res.status(503).json({
-          errorMessage: 'Database error. Please try again later.',
-        });
+        dbErrorResponse(res);
       } else {
-        var generatedQuestions = [];
-        result.rows.forEach((questionTypes) => {
-          generatedQuestions.push(
-            new Promise((resolve) => {
-              queries.getQuestions(questionTypes.type, questionTypes.count,
-                (err, qResult) => {
-                  if (err) throw err;
-                  resolve(qResult.rows);
-                });
-            }));
-        });
+        var generatedQuestions = generateQuestions(result.rows);
         Promise.all(generatedQuestions)
         .then(schema.questionsSchema)
         .then((questions) => {
@@ -70,24 +54,33 @@ function createTemplateController(queries) {
     });
   }
 
-  function handleResponse(err, result, response) {
-    if (err) {
-      response.status(503).json({
-        errorMessage: 'Database error. Please try again later.',
-      });
-    } else {
-      response.status(200).json(schema.allTemplatesSchema(result));
-    }
+  function generateQuestions(types) {
+    var questions = [];
+    types.forEach((questionTypes) => {
+      questions.push(
+        new Promise((resolve) => {
+          queries.getQuestions(questionTypes.type, questionTypes.count,
+            (err, qResult) => {
+              if (err) throw err;
+              resolve(qResult.rows);
+            });
+        }));
+    });
+    return questions;
   }
 
-  function postHandleResponse(err, result, response) {
+  function handleResponse(err, result, response) {
     if (err) {
-      response.status(503).json({
-        errorMessage: 'Database error. Please try again later.',
-      });
+      dbErrorResponse(response);
     } else {
       response.status(200).json(result);
     }
+  }
+
+  function dbErrorResponse(response) {
+    response.status(503).json({
+      errorMessage: 'Database error. Please try again later.',
+    });
   }
 
   return {
