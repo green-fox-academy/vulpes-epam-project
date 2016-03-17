ab79bf74a6ddecbab4c1b0976d6956db4a41da5c
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-03-17 20:28:14 +0100

    move style.scss to a separated /scss subfolder

--- gulp/gulp_sass.js
+++ gulp/gulp_sass.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./client/style/*.scss')
+  return gulp.src('./client/style/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./client/style/css'));
 });
