00802c04273573cf658e6cbc31464ab7654c5475
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-03-17 14:20:44 +0100

    add questions to InterviewFactory

--- client/js/interview/interviewFactory.js
+++ client/js/interview/interviewFactory.js
@@ -4,6 +4,12 @@ var EPAM = require('../main');
 
 EPAM.factory('interview', function ($http) {
     var templateId;
+    var questions = [
+      'What is the difference between call and apply?',
+      'What is the difference between inline and block level elements?',
+      'What is a closure in Javascript?',
+      'What is love?',
+    ];
 
     function setTemplateById(id) {
       templateId = id;
@@ -14,8 +20,13 @@ EPAM.factory('interview', function ($http) {
       return $http.get(url);
     }
 
+    function getQuestions() {
+      return questions;
+    }
+
     return {
       setTemplateById: setTemplateById,
       getInterview: getInterview,
+      getQuestions: getQuestions,
     };
   });
