da1ba875219fdfc06a9603a13a725f86c6e9c2d5
Norbert Pozsonyi <norbert.pozsonyi@gmail.com>
2016-03-17 18:05:27 +0100

    redirect user from interview page when not logged in or no template selected

--- client/js/interview/interviewCtrl.js
+++ client/js/interview/interviewCtrl.js
@@ -2,8 +2,16 @@
 
 var EPAM = require('../main');
 
-EPAM.controller('InterviewCtrl', function ($scope, interview) {
+EPAM.controller('InterviewCtrl', function ($scope, $state, user, interview) {
+  if (!user.isLoggedIn()) {
+    $state.go('login');
+  }
+
   $scope.questions = interview.getQuestions();
+
+  if (!$scope.questions) {
+    $state.go('newInterview');
+  }
 
   $scope.generateNewQuestions = function () {
     interview.getInterview().then(function () {
